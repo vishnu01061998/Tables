89dbe775ace7513bcbfe6f3fb8d6d0ea4e76cfe7
HP <HP@DESKTOP-4Q8V1JF>
2023-03-19 23:07:17 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Tables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the Limit");
+            int limit = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine(i * j);
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
